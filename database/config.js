8531211711c6aea0c8705423931df412bb22bea5
chamodxxc <nimsarawanas.inhe@gmail.com>
2025-12-24 00:01:58 +0530

    Add files via upload

---
+++ database/config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  tokens: "8240669472:AAHBZ9zdqZkg_o9RxQ6CEEhz1AWzNq9nuDw",  // Masukin Bot token kamu
+  owners: "7330881176", // Masukin ID Telegram kamu
+  port: "2008", // Masukin Port panel kamu 
+  ipvps: "kyu-silence.hostingvvip.my.id" // Masukin IP vps kamu atau domain panel kamu yg asalnya ( https://AiiSigma.id ) menjadi ( http://AiiSigma.id )
+};
+
+/* WAJIB BACA !!!
+ cari di index.js halaman 1995-1996 di situ masukin bot token sama id tele lu.
+
+ emngnya knpa bang?
+ lu buka aja dulu. di sana udah gw kasi penjelasannya
+*/
